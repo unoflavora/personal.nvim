8ee9872f4d3a79f413774e7379e313a825002f15
Imam Syahid Hudzaifah <imam.hudzaifah@esb.co.id>
2024-06-07 13:59:33 +0700

    add snippet

--- init.lua
+++ init.lua
@@ -227,6 +227,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  'dcampos/nvim-snippy',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   {
